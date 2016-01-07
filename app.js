c82ab52e44792c6a5cb2875c9ddad02b06b58d81
ybinstock <ybinstock@gmail.com>
2016-01-07 19:21:24 +0700

    Procfile

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-/**
- * Created by ybinstock on 1/7/16.
- */
-var express    = require('express'),
-    app        = express(),
-    server     = require('http').createServer(app),
-    ejs        = require('ejs'),
-    bodyParser = require('body-parser'),
-    io         = require('socket.io').listen(server);
 
-server.listen(process.env.PORT || 5000, function() {
-  console.log('server started');
-});
+
+server.listen(process.env.PORT || 5000);

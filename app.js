394abcaf5d53abf8b8bda1df745a0d33f17501fd
ybinstock <ybinstock@gmail.com>
2016-01-07 19:17:43 +0700

    adding procfile

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 /**
  * Created by ybinstock on 1/7/16.
  */
-var port = process.env.PORT || 3000;
-app.listen(port); 
+var express    = require('express'),
+    app        = express(),
+    server     = require('http').createServer(app),
+    ejs        = require('ejs'),
+    bodyParser = require('body-parser'),
+    io         = require('socket.io').listen(server);
+
+server.listen(process.env.PORT || 5000, function() {
+  console.log('server started');
+});

2353858335dd0a7f0fd72f37396015056fef8edb
ybinstock <ybinstock@gmail.com>
2016-01-09 15:01:14 +0700

    almost there

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 //Middleware
-app.get('/', function(req, res) {
-  res.send("Hello World");
+app.get('index.html', function(req, res) {
+  console.log('this is working')
 });
 
 app.listen(process.env.PORT || 3000, function() {

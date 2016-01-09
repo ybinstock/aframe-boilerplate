b85937907a1e1e395f2f9500fe668c12f4fde483
ybinstock <ybinstock@gmail.com>
2016-01-09 15:41:25 +0700

    why is this not working

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var ejs = require('ejs');
-var app = express();
-
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-app.get('/', function(req, res) {
-  res.render('root');
-  console.log('this is working')
-});
-
-app.listen(process.env.PORT || 3000, function() {
-  console.log("listening on 3000");
-});
+//var express = require('express');
+//var ejs = require('ejs');
+//var app = express();
+//
+//app.engine('html', require('ejs').renderFile);
+//app.set('view engine', 'html');
+//
+//app.get('/', function(req, res) {
+//  res.render('root');
+//  console.log('this is working')
+//});
+//
+//app.listen(process.env.PORT || 3000, function() {
+//  console.log("listening on 3000");
+//});
